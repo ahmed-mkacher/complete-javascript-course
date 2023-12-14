473b7e3a88687461968c7864f88b4fa10ce27a73
Ahmed Mkacher <96886035+ahmed-mkacher@users.noreply.github.com>
2023-12-14 20:59:50 +0100

    Learned about variables

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,4 +1,16 @@
 let js = "amazing";
-if (js === "amazing") alert("JavaScript is FUN!");
 
 console.log(40 + 8 + 23 - 10);
+
+console.log(23);
+
+let firstName = "Matilda";
+
+let jonas_matilda = "JM";
+let $function = 27;
+
+let person = "Jonas";
+let PI = 3.1415;
+
+let myFirstJob = "Coder";
+let mySecondJob = "Teacher";
