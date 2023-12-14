e2d104af22b3c2c624817f093543d598834a4ffc
Ahmed Mkacher <96886035+ahmed-mkacher@users.noreply.github.com>
2023-12-14 22:29:31 +0100

    Completed coding challenge 1

---
+++ 01-Fundamentals-Part-1/starter/challenges.js
@@ -0,0 +1,20 @@
+/*
+// Coding challenge 1
+
+// let massMark= 78;
+// let massJohn= 92;
+// const heightMark= 1.69;
+// const heightJohn= 1.95;
+
+let massMark= 95;
+let massJohn= 85;
+const heightMark= 1.88;
+const heightJohn= 1.76;
+
+const marksBMI= massMark/ heightMark** 2;
+const johnsBMI= massJohn/ heightJohn** 2;
+
+const markHigherBMI= johnsBMI < marksBMI;
+
+console.log(marksBMI, johnsBMI, markHigherBMI);
+*/
