afab48f88c256c2ee7a87caba32d4c0880cfa656
Ahmed Mkacher <96886035+ahmed-mkacher@users.noreply.github.com>
2023-12-17 13:26:36 +0100

    Completed coding challenge 4

--- 01-Fundamentals-Part-1/starter/challenges.js
+++ 01-Fundamentals-Part-1/starter/challenges.js
@@ -58,5 +58,6 @@ const bill = 275;
 // const bill = 40;
 // const bill = 430;
 bill <= 300 && bill >= 50 ? tip = 0.15 * bill : tip = 0.2 * bill;
+console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${tip + bill}`);
 */
 
