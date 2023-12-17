e2f5e1d146f4073906214f1c50c65a8d9aa1e964
Ahmed Mkacher <96886035+ahmed-mkacher@users.noreply.github.com>
2023-12-17 14:00:36 +0100

    Completed functions lecture assignment

---
+++ 02-Fundamentals-Part-2/starter/assignments.js
@@ -0,0 +1,14 @@
+/*
+"use strict";
+
+// Functions lecture assignment
+
+function describeCountry(country, population, capitalCity)
+{
+    return `${country} has ${population} million people and its capital city is ${capitalCity}`;
+}
+
+console.log(describeCountry("Tunisia", 12, "Tunis"));
+console.log(describeCountry("USA", 360, "Washington DC"));
+console.log(describeCountry("China", 1400, "Beijing"));
+*/
