620542cd388d2a0c41d1a1a0814c9a2a2939818e
Ahmed Mkacher <96886035+ahmed-mkacher@users.noreply.github.com>
2023-12-17 13:51:38 +0100

    Completed functions lecture

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,41 @@
+/*
+// Strict mode lecture
+
+"use strict";
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log("I can drive :D");
+
+// const interface = "Audio";
+// const private = 534;
+
+// Functions lecture
+
+function logger()
+{
+    console.log(`My name is Ahmed`);
+}
+
+// calling / running / invoking, a function
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges)
+{
+    console.log(apples, oranges);
+    const juice = `Juice with ${apples} apple and ${oranges} oranges.`;
+    return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+console.log(fruitProcessor(5, 0));
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+*/
+
