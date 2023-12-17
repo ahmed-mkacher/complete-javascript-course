11bff0ec4c22418d693a50ee285946d7b23dd8cd
Ahmed Mkacher <96886035+ahmed-mkacher@users.noreply.github.com>
2023-12-17 15:38:42 +0100

    Completed function declaration vs expression lecture assignment

--- 02-Fundamentals-Part-2/starter/assignments.js
+++ 02-Fundamentals-Part-2/starter/assignments.js
@@ -11,4 +11,28 @@ function describeCountry(country, population, capitalCity)
 console.log(describeCountry("Tunisia", 12, "Tunis"));
 console.log(describeCountry("USA", 360, "Washington DC"));
 console.log(describeCountry("China", 1400, "Beijing"));
+
+// Function declaration vs expression lecture assignment
+
+function percentageOfWorld1(population)
+{
+    return 100 * population / 7900;
+}
+
+const china = percentageOfWorld1(1441);
+const tunisia = percentageOfWorld1(12);
+const usa = percentageOfWorld1(360);
+
+console.log(china);
+console.log(tunisia);
+console.log(usa);
+
+const percentageOfWorld2 = function(population)
+{
+    return 100 * population / 7900;
+}
+
+console.log(percentageOfWorld2(1442));
+console.log(percentageOfWorld2(360));
+console.log(percentageOfWorld2(12));
 */
