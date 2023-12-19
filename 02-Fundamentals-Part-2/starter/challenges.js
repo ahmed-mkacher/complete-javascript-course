ce50c47a5f901d3756eaf3d35cdf2f23c776455d
Ahmed Mkacher <96886035+ahmed-mkacher@users.noreply.github.com>
2023-12-19 21:51:56 +0100

    Completed coding challenge 1

---
+++ 02-Fundamentals-Part-2/starter/challenges.js
@@ -0,0 +1,29 @@
+
+"use strict";
+
+const dS1 = 85;
+const dS2 = 54;
+const dS3 = 41;
+
+const kS1 = 23;
+const kS2 = 34;
+const kS3 = 27;
+
+const calcAverage = (score1, score2, score3) => (score1 + score2 + score3) / 3;
+
+const avgDolphins = calcAverage(dS1, dS2, dS3);
+const avgKoalas = calcAverage(kS1, kS2, kS3);
+
+function checkWinner(avgDolphins, avgKoalas)
+{
+    if (avgDolphins >= 2 * avgKoalas)
+    {
+        console.log(`Dolphins win (${avgDolphins} vs. ${avgKoalas})`);
+    }
+    else if (avgKoalas >= 2 * avgDolphins)
+    {
+        console.log(`Koalas win (${avgKoalas} vs. ${avgDolphins})`);
+    }
+}
+
+checkWinner(avgDolphins, avgKoalas);
